928337de8f0e6808803a8f9396ad73458998414f
eric <eric.ganzert@gmail.com>
2019-07-30 16:29:31 -0700

    adding debug and test targets exluded from all

---
+++ debug.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	printf("debug tool\n");
+	return 0;
+}
---
+++ test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	printf("testing project\n");
+	return 0;
+}
